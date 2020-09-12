9864a383d71e49caa37b5a0ab16a877ddb8f545d
Chi Lam <theonlyasian.dc@gmail.com>
2020-09-11 23:44:59 -0400

    finished model

--- P05_ModelBuilding.py
+++ P05_ModelBuilding.py
@@ -7,12 +7,13 @@ Created on Tue Aug 18 21:10:41 2020
 
 #Import module
 import pandas as pd
+import seaborn as sns
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
 from kneed import KneeLocator
 import matplotlib.pyplot as plt
 %matplotlib inline
-
+pd.set_option('display.max_columns', 100)
 
 #Read in data
 df_player = pd.read_csv('player_cleaned.csv')
@@ -68,3 +69,56 @@ frame['cluster'].value_counts()
 ##Scatter plot
 plt.figure(figsize=(12,6))
 plt.scatter(df_player['rank'], frame['cluster'])
+
+
+#Examine clusters
+##Correlation Heat Map
+df_player['cluster'] = frame['cluster']
+
+corr = df_player.corr()
+plt.subplots(figsize=(15, 12))
+sns.heatmap(corr, vmax = .8, square = True, annot = True, cmap = "YlGnBu")
+plt.title('Figure n: variables correlations heatmap', fontsize=15)
+
+##Look at each cluster
+plt.subplots(figsize=(15, 23))
+
+plt.subplot(421)
+plt.title('Figure n: "match" statistics of each cluster', fontsize=15)
+sns.boxplot(x = df_player['cluster'], y = df_player['match'])
+
+plt.subplot(422)
+plt.title('Figure n: "damages_per_match" statistics of each cluster', fontsize=15)
+sns.boxplot(x = df_player['cluster'], y = df_player['damages_per_match'])
+
+plt.subplot(423)
+plt.title('Figure n: "kill_per_match" statistics of each cluster', fontsize=15)
+sns.boxplot(x = df_player['cluster'], y = df_player['kill_per_match'])
+
+plt.subplot(424)
+plt.title('Figure n: "survive" statistics of each cluster', fontsize=15)
+sns.boxplot(x = df_player['cluster'], y = df_player['survive'])
+
+plt.subplot(425)
+plt.title('Figure n: "assist" statistics of each cluster', fontsize=15)
+sns.boxplot(x = df_player['cluster'], y = df_player['assist'])
+
+plt.subplot(426)
+plt.title('Figure n: "longest" statistics of each cluster', fontsize=15)
+sns.boxplot(x = df_player['cluster'], y = df_player['longest'])
+
+plt.subplot(427)
+plt.title('Figure n: "traveled" statistics of each cluster', fontsize=15)
+sns.boxplot(x = df_player['cluster'], y = df_player['traveled'])
+
+plt.subplot(428)
+plt.title('Figure n: "accuracy" statistics of each cluster', fontsize=15)
+sns.boxplot(x = df_player['cluster'], y = df_player['accuracy'])
+
+##clusters of each team
+plt.subplots(figsize=(10, 6))
+plt.title('Figure n: different cluster in each team', fontsize=15)
+plt.scatter(df_player['team_placement'], df_player['cluster'], s=100, alpha=0.5)
+plt.xlabel('Team Placement')
+plt.ylabel('Cluster')
+plt.grid()
